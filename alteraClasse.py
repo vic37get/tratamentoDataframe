97c50df637e5bf8a2087ada493933efcbc28ec4d
Victor Ribeiro da Silva <victorribeiro172@gmail.com>
2022-09-26 11:57:28 -0300

    alteração alteraClasse

--- alteraClasse.py
+++ alteraClasse.py
@@ -6,6 +6,14 @@ global bar
 from tqdm import tqdm
 
 
+
+def alteraClasse(dataframe):
+    colunas = dataframe.columns
+    dataframe.fillna('',inplace=True)
+    bar = tqdm(total=dataframe.shape[0])
+    dados = dataframe.apply(filter_replace,axis=1)
+    return dados
+
 def filter_replace(row):
     for indice, elemento in enumerate(row):
         nome_coluna = colunas[indice]
@@ -14,10 +22,3 @@ def filter_replace(row):
                 row[indice-1] = 0
     bar.update(1)
     return row
-
-def alteraClasse(dataframe):
-    colunas = dataframe.columns
-    dataframe.fillna('',inplace=True)
-    bar = tqdm(total=dataframe.shape[0])
-    dados = dataframe.apply(filter_replace,axis=1)
-    return dados
